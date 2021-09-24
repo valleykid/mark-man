bb2cd4298ca0189c4f6145a03f7370edcfea6540
valleykid <valleykiddy@gmail.com>
2021-09-24 09:27:38 +0800

    v0.0.4

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,10 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 import typescript from 'rollup-plugin-typescript';
 import { uglify } from 'rollup-plugin-uglify';
+import dts from "rollup-plugin-dts";
 import pkg from './package.json';
 
-export default {
+export default [{
   input: 'src/index.ts',
   output: {
     name: 'markMan',
@@ -17,4 +18,8 @@ export default {
     typescript(),
     uglify()
   ]
-}; 
+}, {
+  input: 'src/index.ts',
+  output: { file: './lib/index.d.ts', format: 'es' },
+  plugins: [dts()]
+}]; 
